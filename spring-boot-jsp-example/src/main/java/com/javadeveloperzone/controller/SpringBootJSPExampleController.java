c3c085cb86630a021b03c9959ebcb7d8564f19b2
preetas <88144614+preetas@users.noreply.github.com>
2021-08-13 00:18:44 +0530

    Update SpringBootJSPExampleController.java

--- spring-boot-jsp-example/src/main/java/com/javadeveloperzone/controller/SpringBootJSPExampleController.java
+++ spring-boot-jsp-example/src/main/java/com/javadeveloperzone/controller/SpringBootJSPExampleController.java
@@ -13,7 +13,7 @@ import org.springframework.boot.SpringApplication;
 @Controller
 public class SpringBootJSPExampleController {
 
-    @RequestMapping("welcome") // it will handle all request for /welcome
+    @GetMapping("/welcome") // it will handle all request for /welcome
     public String SpringBootHello() {
          return "welcome";  // welcome is view name. It will call welcome.jsp
     }
