964dc54de41ddba60764c7fa7f65f136ae77a3de
preetas <88144614+preetas@users.noreply.github.com>
2021-08-12 22:52:42 +0530

    Update SpringBootJSPExampleController.java

--- spring-boot-jsp-example/src/main/java/com/javadeveloperzone/controller/SpringBootJSPExampleController.java
+++ spring-boot-jsp-example/src/main/java/com/javadeveloperzone/controller/SpringBootJSPExampleController.java
@@ -3,7 +3,7 @@ package com.javadeveloperzone.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.boot.SpringApplication;
 /**
  * Created by Lenovo on 19-07-2017.
  */
