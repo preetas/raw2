4364c48e3afda7ca327cd3eef37ede7998699fd4
preetas <88144614+preetas@users.noreply.github.com>
2021-08-12 17:20:17 +0530

    Add files via upload

---
+++ spring-boot-jsp-example/src/main/java/com/javadeveloperzone/SpringBootJSPExampleController.java
@@ -0,0 +1,16 @@
+package com.javadeveloperzone.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by Lenovo on 19-07-2017.
+ */
+@Controller
+public class SpringBootJSPExampleController {
+
+    @GetMapping("/welcome")                     // it will handle all request for /welcome
+    public String SpringBootHello() {
+        return "welcome";           // welcome is view name. It will call welcome.jsp
+    }
+}
