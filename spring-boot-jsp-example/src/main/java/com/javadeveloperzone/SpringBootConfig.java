93b3a7542e79233bdf9052a7423140f964243961
preetas <88144614+preetas@users.noreply.github.com>
2021-08-24 18:37:53 +0530

    Update SpringBootConfig.java

--- spring-boot-jsp-example/src/main/java/com/javadeveloperzone/SpringBootConfig.java
+++ spring-boot-jsp-example/src/main/java/com/javadeveloperzone/SpringBootConfig.java
@@ -1,14 +1,8 @@
 package com.javadeveloperzone;
 
-import org.springframework.boot.SpringApplication;
+/*import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-
-/**
- * Created by JavaDeveloperZone on 19-07-2017.
- */
+import 
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.javadeveloperzone.controller"})
@@ -22,4 +16,4 @@ public class SpringBootConfig extends SpringBootServletInitializer {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SpringBootConfig.class, args);            // it wil start application
     }
-}
+}*/
