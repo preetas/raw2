288759b7fdab278137736ff31217f7cae91f3f67
preetas <88144614+preetas@users.noreply.github.com>
2021-08-24 19:01:49 +0530

    Update SpringBootConfig.java

--- spring-boot-jsp-example/src/main/java/com/javadeveloperzone/SpringBootConfig.java
+++ spring-boot-jsp-example/src/main/java/com/javadeveloperzone/SpringBootConfig.java
@@ -1,5 +1,11 @@
 package com.javadeveloperzone;
 
+import org.springframework.boot.SpringApplication; import org.springframework.boot.autoconfigure.SpringBootApplication; 
+ @SpringBootApplication public class WebMvcConfigExample 
+{  public static void main (String[] args)
+ {  SpringApplication app = new SpringApplication(WebMvcConfigExample.class); 
+app.run(args); } }
+
 /*import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import 
