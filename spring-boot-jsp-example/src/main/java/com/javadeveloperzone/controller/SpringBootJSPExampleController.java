4465bfed3d98db592c2f687363f803da26d1c825
preetas <88144614+preetas@users.noreply.github.com>
2021-08-24 16:21:35 +0530

    Update SpringBootJSPExampleController.java

--- spring-boot-jsp-example/src/main/java/com/javadeveloperzone/controller/SpringBootJSPExampleController.java
+++ spring-boot-jsp-example/src/main/java/com/javadeveloperzone/controller/SpringBootJSPExampleController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.ComponentScan;
