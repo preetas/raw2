49bbd0897766d39fc6689952f3a6a6306a501694
preetas <88144614+preetas@users.noreply.github.com>
2021-08-26 22:10:43 +0530

    Update WebMvcConfigExample.java

--- spring-boot-jsp-example/src/main/java/com/javadeveloperzone/WebMvcConfigExample.java
+++ spring-boot-jsp-example/src/main/java/com/javadeveloperzone/WebMvcConfigExample.java
@@ -2,6 +2,8 @@ package com.javadeveloperzone;
 
 import org.springframework.boot.SpringApplication; import org.springframework.boot.autoconfigure.SpringBootApplication; 
 
+import org.springframework.context.annotation.Profile;
+
 @Profile("profile1")
 @SpringBootApplication 
 public class WebMvcConfigExample 
