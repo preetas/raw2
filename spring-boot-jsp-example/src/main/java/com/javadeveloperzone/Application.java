01a03e0a2186a92c8ed6e52058cea65099881039
preetas <88144614+preetas@users.noreply.github.com>
2021-08-27 08:42:52 +0530

    Update Application.java

--- spring-boot-jsp-example/src/main/java/com/javadeveloperzone/Application.java
+++ spring-boot-jsp-example/src/main/java/com/javadeveloperzone/Application.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-@Configuration
+//@Configuration
 @ComponentScan(basePackages = "com.javadeveloperzone.controller")
 //@EnableWebMvc
 @EnableAutoConfiguration
