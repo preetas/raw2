34d49e4fd5fed20d5bf4f6f9bfd47086491feac8
preetas <88144614+preetas@users.noreply.github.com>
2021-08-28 11:31:06 +0530

    Update Application.java

--- spring-boot-jsp-example/src/main/java/com/javadeveloperzone/Application.java
+++ spring-boot-jsp-example/src/main/java/com/javadeveloperzone/Application.java
@@ -1,28 +1,21 @@
 package com.javadeveloperzone;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
-@Configuration
-@ComponentScan(basePackages = "com.javadeveloperzone.controller")
-//@EnableWebMvc
-@EnableAutoConfiguration
+@SpringBootApplication
+@ComponentScan(basePackages= {"com.example.mvc"})
 public class Application extends SpringBootServletInitializer {
-	
-	 private static Class applicationClass = Application.class;
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(applicationClass);
-    }
-
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
 }
